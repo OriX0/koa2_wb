a8e5f3e758307c4047fe5d42b223e4d56578edc3
OriX <1098888910@qq.com>
2021-05-21 16:27:55 +0800

    📦 feat:增加session配置

--- src/app.js
+++ src/app.js
@@ -1,38 +1,61 @@
 /*
- * @description: 
+ * @description:
  * @Author: OriX
  * @Date: 2021-05-13 20:49:53
  * @LastEditors: OriX
- * @LastEditTime: 2021-05-13 21:52:42
+ * @LastEditTime: 2021-05-21 13:41:17
  */
-const Koa = require('koa')
-const app = new Koa()
-const views = require('koa-views')
-const json = require('koa-json')
-const onerror = require('koa-onerror')
-const bodyparser = require('koa-bodyparser')
-const logger = require('koa-logger')
-
-const index = require('./routes/index')
-const users = require('./routes/users')
+const Koa = require('koa');
+const app = new Koa();
+const views = require('koa-views');
+const json = require('koa-json');
+const onerror = require('koa-onerror');
+const bodyparser = require('koa-bodyparser');
+const logger = require('koa-logger');
+const session = require('koa-generic-session');
+const redisStore = require('koa-redis');
+const index = require('./routes/index');
+const users = require('./routes/users');
+// 引入 用于session持久化的redis 配置
+const { REDIS_CONF } = require('./conf/db');
 
 // error handler
-onerror(app)
+onerror(app);
 
 // middlewares
-app.use(bodyparser({
-  enableTypes: ['json', 'form', 'text']
-}))
-app.use(json())
-app.use(logger())
+app.use(
+  bodyparser({
+    enableTypes: ['json', 'form', 'text'],
+  })
+);
+app.use(json());
+app.use(logger());
 // 使用静态文件解析中间件
-app.use(require('koa-static')(__dirname + '/public'))
+app.use(require('koa-static')(__dirname + '/public'));
 
 // 使用ejs引擎
-app.use(views(__dirname + '/views', {
-  extension: 'pug'
-}))
-
+app.use(
+  views(__dirname + '/views', {
+    extension: 'pug',
+  })
+);
+// session配置
+app.keys = ['Orix_192_51#']; // 加密秘钥
+app.use(
+  session({
+    key: 'weibo_sid', // cookies的名字
+    prefix: 'weibo:sess', // redis key 的前缀 默认是koa sess
+    cookie: {
+      path: '/',
+      httpOnly: true, // 是否只能后端操作session
+      maxAge: 24 * 60 * 60 * 1000, // cookie的过期时间 同时也会设置ttl（redis）的过期时间 默认和这个一样
+    },
+    // ttl:24*60*60*1000
+    store: redisStore({
+      all: `${REDIS_CONF.host}:${REDIS_CONF.port}`,
+    }),
+  })
+);
 // logger 相当于演示手写logger
 /* app.use(async (ctx, next) => {
   const start = new Date()
@@ -42,12 +65,12 @@ app.use(views(__dirname + '/views', {
 }) */
 
 // routes
-app.use(index.routes(), index.allowedMethods())
-app.use(users.routes(), users.allowedMethods())
+app.use(index.routes(), index.allowedMethods());
+app.use(users.routes(), users.allowedMethods());
 
 // error-handling
 app.on('error', (err, ctx) => {
-  console.error('server error', err, ctx)
+  console.error('server error', err, ctx);
 });
 
-module.exports = app
+module.exports = app;
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,19 +1,33 @@
-const router = require('koa-router')()
+/*
+ * @Description:
+ * @Author: OriX
+ * @Date: 2021-05-14 14:58:40
+ * @LastEditors: OriX
+ * @LastEditTime: 2021-05-21 13:45:56
+ */
+const router = require('koa-router')();
 
 router.get('/', async (ctx, next) => {
   await ctx.render('index', {
-    title: 'Hello Koa 2!'
-  })
-})
+    title: 'Hello Koa 2!',
+  });
+});
 
 router.get('/string', async (ctx, next) => {
-  ctx.body = 'koa2 string'
-})
+  ctx.body = 'koa2 string';
+});
 
 router.get('/json', async (ctx, next) => {
-  ctx.body = {
-    title: 'koa2 json'
+  // 使用session 以触发 session持久化
+  const session = ctx.session;
+  if (session.viewNum == null) {
+    session.viewNum = 0;
   }
-})
+  session.viewNum++;
+  ctx.body = {
+    title: 'koa2 json',
+    viewNum: session.viewNum,
+  };
+});
 
-module.exports = router
+module.exports = router;
