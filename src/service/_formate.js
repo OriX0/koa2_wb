5bfdb9abe77d3aa838de5281fbab1d4f780839fd
OriX0 <1098888910@qq.com>
2021-05-25 21:56:54 +0800

    🐛 fix:修复默认头像未正常赋值的bug

--- src/service/_formate.js
+++ src/service/_formate.js
@@ -2,17 +2,17 @@
  * @Description: 格式化处理数据
  * @Author: OriX
  * @LastEditors: OriX
- * @LastEditTime: 2021-05-22 21:38:08
+ * @LastEditTime: 2021-05-25 21:55:46
  */
 const { DEFAULT_PICTURE_URL } = require('../conf/constant');
 /**
- * 当picurl为空的时候自动赋能
+ * 当pic url为空的时候自动赋能
  * @param {object} userObj 用户对象
  * @returns
  */
-function _formatePicUrl(userObj) {
+function _formatePicUrl (userObj) {
   if (userObj.picture === null) {
-    userObj.picture === DEFAULT_PICTURE_URL;
+    userObj.picture = DEFAULT_PICTURE_URL;
   }
   return userObj;
 }
@@ -20,7 +20,7 @@ function _formatePicUrl(userObj) {
  * 批量或者单个更新user的信息
  * @param {Object|Array} list
  */
-function formateUser(list) {
+function formateUser (list) {
   if (list === null) {
     return list;
   }
