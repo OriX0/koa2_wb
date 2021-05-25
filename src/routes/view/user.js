2f662f24f78f0ccfb61606fae678ab7024195bc0
OriX0 <1098888910@qq.com>
2021-05-25 22:02:39 +0800

    📦 feat:添加设置相关的ui和视图路由

--- src/routes/view/user.js
+++ src/routes/view/user.js
@@ -2,7 +2,7 @@
  * @Description: user相关操作的路由
  * @Author: OriX
  * @LastEditors: OriX
- * @LastEditTime: 2021-05-24 21:16:40
+ * @LastEditTime: 2021-05-25 21:45:40
  */
 const router = require('koa-router')();
 const { loginRedirect } = require('../../middleware/loginChecks');
@@ -11,7 +11,7 @@ const { loginRedirect } = require('../../middleware/loginChecks');
  * @param {ctx} ctx
  * @returns
  */
-function getLoginInfo(ctx) {
+function getLoginInfo (ctx) {
   let data = {
     isLogin: false,
   };
@@ -31,7 +31,7 @@ router.get('/login', async (ctx, next) => {
   await ctx.render('login', getLoginInfo(ctx));
 });
 router.get('/setting', loginRedirect, async (ctx, next) => {
-  // 暂时占位 测试 重定向中间件
-  await ctx.render('index');
+  //  用户设置页
+  await ctx.render('setting', ctx.session.userInfo);
 });
 module.exports = router;
