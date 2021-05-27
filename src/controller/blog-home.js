4ce1c8310f55413baacad2e638dbc28d95c40006
OriX <1098888910@qq.com>
2021-05-27 15:23:03 +0800

    🎨 style:将创建微博的值从控制层返回

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -2,7 +2,7 @@
  * @Description: blog 控制层
  * @Author: OriX
  * @LastEditors: OriX
- * @LastEditTime: 2021-05-27 15:02:50
+ * @LastEditTime: 2021-05-27 15:22:00
  */
 const xss = require('xss');
 const { SuccessModel, ErrorModel } = require('../model/ResModel');
@@ -17,12 +17,12 @@ const { createBlogFailInfo } = require('../model/ErrorInfo');
 async function create({ userId, content, image }) {
   console.log(userId, content, image);
   try {
-    await createBlog({
+    const blog = await createBlog({
       userId,
       content: xss(content),
       image,
     });
-    return new SuccessModel();
+    return new SuccessModel(blog);
   } catch (error) {
     console.log(error.messgae, error.stack);
     return new ErrorModel(createBlogFailInfo);
