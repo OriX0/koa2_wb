1a928de0165710ceb3829e96aed36b035692027e
OriX <1098888910@qq.com>
2021-05-27 15:04:25 +0800

    📦 feat:xss过滤

--- src/controller/blog-home.js
+++ src/controller/blog-home.js
@@ -2,11 +2,13 @@
  * @Description: blog 控制层
  * @Author: OriX
  * @LastEditors: OriX
- * @LastEditTime: 2021-05-27 14:17:19
+ * @LastEditTime: 2021-05-27 15:02:50
  */
+const xss = require('xss');
 const { SuccessModel, ErrorModel } = require('../model/ResModel');
 const { createBlog } = require('../service/blog');
 const { createBlogFailInfo } = require('../model/ErrorInfo');
+
 /**
  * 创建微博
  * @param {object} param0 所需要的数据 userId, content, image
@@ -17,7 +19,7 @@ async function create({ userId, content, image }) {
   try {
     await createBlog({
       userId,
-      content,
+      content: xss(content),
       image,
     });
     return new SuccessModel();
