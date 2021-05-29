ddd7565b675c55c6ac73769e95c5ac8771e3d171
OriX <1098888910@qq.com>
2021-05-29 16:03:09 +0800

    📦 chore:更改关于followerId的注释 使其理解起来更明确

--- src/db/model/userRelation.js
+++ src/db/model/userRelation.js
@@ -2,7 +2,7 @@
  * @Description: 用户 关系数据模型
  * @Author: OriX
  * @LastEditors: OriX
- * @LastEditTime: 2021-05-29 13:57:36
+ * @LastEditTime: 2021-05-29 16:01:48
  */
 
 const seq = require('../seq');
@@ -16,7 +16,7 @@ const UserRelation = seq.define('userRelation', {
   followerId: {
     type: INTEGER,
     allowNull: false,
-    comment: '被关注的 用户的id',
+    comment: '这个用户关注的人的ID',
   },
 });
 
