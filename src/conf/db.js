8d0405036494c46aa9b2d056d78f679a8cb7eb40
OriX <1098888910@qq.com>
2021-06-02 19:58:12 +0800

    📦 feat:数据库配置脱敏

--- src/conf/db.js
+++ src/conf/db.js
@@ -3,7 +3,7 @@
  * @Author: OriX
  * @Date: 2021-05-20 17:41:45
  * @LastEditors: OriX
- * @LastEditTime: 2021-05-25 17:30:35
+ * @LastEditTime: 2021-06-02 19:57:44
  */
 const { isProd } = require('../utils/env');
 let REDIS_CONF = {
@@ -14,8 +14,8 @@ let MYSQL_CONF = {
   host: '127.0.0.1',
   prot: '3306',
   user: 'root',
-  password: '12345wszcx',
-  database: 'koa2_wb_db',
+  password: '你的密码',
+  database: '你的数据库名',
 };
 if (isProd) {
   // 如果是生产环境 则使用生产环境下的配置
@@ -27,8 +27,8 @@ if (isProd) {
     host: '127.0.0.1',
     prot: '3306',
     user: 'root',
-    password: '',
-    database: 'koa_wb_db',
+    password: '你的密码',
+    database: '你的数据库名',
     pool: {
       max: 5,
       min: 0,
